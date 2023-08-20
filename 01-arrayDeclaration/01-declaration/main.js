478e0a14a390a337024083e0561fa8fd2b0556c9
Nitradee8 <nbtt.nik@gmail.com>
2023-08-20 22:23:20 +0700

    update

---
+++ 01-arrayDeclaration/01-declaration/main.js
@@ -0,0 +1,9 @@
+// จงสร้างตัวแปร Array เพื่อเก็บชื่อของเพื่อนจำนวน 5 ชื่อ
+
+
+const friends = ['Pang','Arm', 'Som', 'Plum', 'Tik'];
+console.log(friends[0]);
+console.log(friends[1]);
+console.log(friends[2]);
+console.log(friends[3]);
+console.log(friends[4]);
