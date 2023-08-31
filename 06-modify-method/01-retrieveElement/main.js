47bc2bf40ef05d40985b2ef299c43e97d8a9ed75
Nitradee8 <nbtt.nik@gmail.com>
2023-08-31 23:13:15 +0700

    update

---
+++ 06-modify-method/01-retrieveElement/main.js
@@ -0,0 +1,12 @@
+// ให้สร้างตัวแปร Array โดยเอาค่ามาจาก sports ในตำแหน่ง index 1 ถึง 3
+
+
+const sports = ['football', 'volleyball', 'tennis', 'basketball', 'judo'];
+
+// let arr = ['football', 'volleyball', 'tennis', 'basketball', 'judo'];
+// arr.splice(1,3);
+// console.log(arr);
+
+// sports.splice(1, 3); // sports = ['football','judo']
+
+const removedSports = sports.splice(1, 3);
---
+++ 06-modify-method/02-removeFish/main.js
@@ -0,0 +1,7 @@
+// ให้เขียนโค้ดเพื่อ remove element ในตัวแปร fish ออกให้หมด
+
+
+let fish = ['angel', 'clown', 'mandarin', 'sturgeon'];
+
+// fish.length = 4
+fish.splice(0, fish.length);
