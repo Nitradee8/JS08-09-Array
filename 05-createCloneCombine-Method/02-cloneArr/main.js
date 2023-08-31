803a0a8661e2d44cbbbc963c057b2979d77e7366
Nitradee8 <nbtt.nik@gmail.com>
2023-08-31 23:09:43 +0700

    update

---
+++ 05-createCloneCombine-Method/02-cloneArr/main.js
@@ -0,0 +1,13 @@
+// จงสร้างฟังก์ชัน clone(arr) เพื่อ return ค่า Array ที่มี element เหมือน arr ทุกตัว
+
+
+function clone(arr) {
+    // const newArr = arr.slice()
+    // return newArr
+    return arr.slice();
+}
+
+const a = clone([1, 2, 3, 4]);
+console.log(a);
+a.push(5);
+console.log(a);
---
+++ 05-createCloneCombine-Method/03-retrieveElement/main.js
@@ -0,0 +1,9 @@
+// ให้สร้างตัวแปร Array โดยเอาค่ามาจาก sports ในตำแหน่ง index 1 ถึง 3
+
+
+const sports = ['football', 'volleyball', 'tennis', 'basketball', 'judo'];
+
+const newArr = sports.slice(1, 4);
+
+
+
