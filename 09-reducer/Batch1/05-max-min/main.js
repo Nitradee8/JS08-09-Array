df68e7b9446d75ca3e440381428109865c21213c
Nitradee8 <nbtt.nik@gmail.com>
2023-09-05 18:15:56 +0700

    update

---
+++ 09-reducer/Batch1/05-max-min/main.js
@@ -0,0 +1,19 @@
+// จงหาค่ามากสุดและน้อยสุดใน arr
+// ```js
+// let arr = [3.24, 2.78, 3.86, 1.37, 0.52];
+// ```
+
+// Max
+let arr1 = [3.24, 2.78, 3.86, 1.37, 0.52];
+const max = Math.max(...arr1);
+
+console.log(max); // 3.86
+
+// Min
+let arr2 = [3.24, 2.78, 3.86, 1.37, 0.52];
+const min = Math.min(...arr2);
+
+console.log(min); // 0.52
+
+
+
